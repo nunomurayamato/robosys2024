21a3897f8f63cce6d3d52b69f34f689e7507b7df
Yamato Nunomura <s23c1103bp@s.chibakoudai.jp>
2024-10-31 12:58:48 +0900

    テスト

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Yamato Nunomura
 ng () {
 	 echo ${1}行目が違うよ
@@ -6,9 +6,18 @@ ng () {
 }
 
 res=0
-
+### 正常 ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+### 変な状態 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
